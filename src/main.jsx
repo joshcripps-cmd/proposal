5886028d724b257f3babdc3b339d618492b997c0
joshcripps-cmd <josh.cripps@roccabellayachts.com>
2026-03-26 10:52:22 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import ProposalViewer from './components/ProposalViewer';
 import AdminDashboard from './components/AdminDashboard';
 
@@ -14,15 +14,8 @@ function App() {
         {/* Admin dashboard */}
         <Route path="/admin/*" element={<AdminDashboard />} />
         
-        {/* Default redirect */}
-        <Route path="*" element={
-          <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', minHeight: '100vh', fontFamily: 'DM Sans, sans-serif' }}>
-            <div style={{ textAlign: 'center' }}>
-              <img src="/logo.png" alt="Roccabella Yachts" style={{ width: 200, marginBottom: 24 }} />
-              <p style={{ color: '#64748b' }}>Proposal system active.</p>
-            </div>
-          </div>
-        } />
+        {/* Root redirects to admin */}
+        <Route path="*" element={<Navigate to="/admin" replace />} />
       </Routes>
     </BrowserRouter>
   );
