911c20c83f08bf564f55b2005eba9606726673ff
joshcripps-cmd <josh.cripps@roccabellayachts.com>
2026-03-29 21:13:10 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AdminDashboard from "./components/AdminDashboard";
+import ProposalViewer from "./components/ProposalViewer";
 
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/admin/*" element={<AdminDashboard />} />
+        <Route path="/p/:slug" element={<ProposalViewer />} />
+        <Route path="/" element={<AdminDashboard />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
